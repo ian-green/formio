e3c170759a14692283f44b71193d5c61e7825dc0
Yury Rybak <Yury.Rybak@Softensity.com>
2017-12-29 12:42:01 +0300

    WIP: Refactoring.

--- src/middleware/filterResourcejsResponse.js
+++ src/middleware/filterResourcejsResponse.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var _ = require('lodash');
-var debug = require('debug')('formio:middleware:filterResourcejsResponse');
+const _ = require('lodash');
+const debug = require('debug')('formio:middleware:filterResourcejsResponse');
 
 /**
  * Middleware function to filter the response from resourcejs.
@@ -11,49 +11,45 @@ var debug = require('debug')('formio:middleware:filterResourcejsResponse');
  *
  * @returns {Function}
  */
-module.exports = function(router) {
-  return function(settings) {
-    return function(req, res, next) {
-      if (!settings || settings === [] || !_.isArray(settings)) {
-        return next();
-      }
-      if (!res || !res.resource || !res.resource.item) {
-        return next();
-      }
+module.exports = (router) => (settings) => (req, res, next) => {
+  if (!settings || settings === [] || !_.isArray(settings)) {
+    return next();
+  }
+  if (!res || !res.resource || !res.resource.item) {
+    return next();
+  }
 
-      debug(settings);
-      // Merge all results into an array, to handle the cases with multiple results.
-      var multi = false;
-      var list = [].concat(res.resource.item);
-      if (_.isArray(res.resource.item)) {
-        multi = true;
-      }
+  debug(settings);
+  // Merge all results into an array, to handle the cases with multiple results.
+  let multi = false;
+  const list = [].concat(res.resource.item);
+  if (_.isArray(res.resource.item)) {
+    multi = true;
+  }
 
-      // Iterate each provided filter.
-      for (var a = 0; a < settings.length; a++) {
-        // Iterate each result.
-        for (var b = 0; b < list.length; b++) {
-          // Change the response object from a mongoose model to a js object.
-          if (list[b].constructor.name === 'model') {
-            list[b] = list[b].toObject();
-          }
-          // Remove the key if found.
-          if (list[b].hasOwnProperty(settings[a])) {
-            debug('Removing: ' + settings[a]);
-            list[b] = _.omit(list[b], settings[a]);
-          }
-        }
+  // Iterate each provided filter.
+  for (let a = 0; a < settings.length; a++) {
+    // Iterate each result.
+    for (let b = 0; b < list.length; b++) {
+      // Change the response object from a mongoose model to a js object.
+      if (list[b].constructor.name === 'model') {
+        list[b] = list[b].toObject();
       }
+      // Remove the key if found.
+      if (list[b].hasOwnProperty(settings[a])) {
+        debug('Removing: ' + settings[a]);
+        list[b] = _.omit(list[b], settings[a]);
+      }
+    }
+  }
 
-      // If there were multiple results, update the response list, otherwise return only the original item.
-      if (multi) {
-        res.resource.item = list;
-        return next();
-      }
-      else {
-        res.resource.item = list[0];
-        return next();
-      }
-    };
-  };
+  // If there were multiple results, update the response list, otherwise return only the original item.
+  if (multi) {
+    res.resource.item = list;
+    return next();
+  }
+  else {
+    res.resource.item = list[0];
+    return next();
+  }
 };
--- src/middleware/formLoader.js
+++ src/middleware/formLoader.js
@@ -8,19 +8,17 @@
  * @param next
  * @returns {*}
  */
-module.exports = function(router) {
-  return function formLoader(req, res, next) {
-    // Only process on GET request, and if they provide full query.
-    if (
-      req.method !== 'GET' ||
-      !req.query.full ||
-      !res.resource ||
-      !res.resource.item
-    ) {
-      return next();
-    }
+module.exports = (router) => (req, res, next) => {
+  // Only process on GET request, and if they provide full query.
+  if (
+    req.method !== 'GET' ||
+    !req.query.full ||
+    !res.resource ||
+    !res.resource.item
+  ) {
+    return next();
+  }
 
-    // Load all subforms recursively.
-    router.formio.cache.loadSubForms(res.resource.item, req, next);
-  };
+  // Load all subforms recursively.
+  router.formio.cache.loadSubForms(res.resource.item, req, next);
 };
